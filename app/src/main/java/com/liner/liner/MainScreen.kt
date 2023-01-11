b3ac7ffd004f09c5265b56e281f7290af2f06614
jibminJung <jibminjung@gmail.com>
2023-01-11 18:34:47 +0900

    implement movable card

--- app/src/main/java/com/liner/liner/MainScreen.kt
+++ app/src/main/java/com/liner/liner/MainScreen.kt
@@ -1,39 +1,48 @@
 package com.liner.liner
 
-import androidx.compose.animation.core.Animatable
+import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.drag
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 val TAG = "MainScreen"
 
 
 @Composable
 fun MainScreen() {
-    val list = listOf(painterResource(id = R.drawable.pichu),
-        painterResource(id = R.drawable.dummy_image)
-    )
+    val a = painterResource(id = R.drawable.pichu)
+    val b = painterResource(id = R.drawable.dummy_image)
+    val list = remember {
+        mutableStateOf(
+            mutableListOf(
+                a, b
+            )
+        )
+    }
     Box(modifier = Modifier.fillMaxSize()) {
-        for (img in list.reversed()) {
-            MovableCard(modifier = Modifier.align(Alignment.Center),img)
+        for (index in list.value.size -1 downTo 0) {
+            MovableCard(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .swipeAway({}, {}, {}, {
+                        list.value = list.value.drop(1).toMutableList()
+                        Log.d(TAG, "MainScreen: right")
+                        Log.d(TAG, "MainScreen: $list")
+                    }), list.value[index]
+            )
         }
     }
 }
@@ -41,53 +50,8 @@ fun MainScreen() {
 @Composable
 fun MovableCard(modifier: Modifier = Modifier, painterResource: Painter) {
 
-    val offsetX = remember { Animatable(0f) }
-    val offsetY = remember { Animatable(0f) }
-    val rotation = remember { Animatable(0f) }
-    var isTouchOnUpperSide by remember { mutableStateOf(false) }
-
     Card(
         modifier = modifier
-            .graphicsLayer(
-                translationX = offsetX.value,
-                translationY = offsetY.value,
-                rotationZ = rotation.value
-            )
-            .pointerInput(Unit) {
-                val centerHeight = this.size.height / 2
-
-                detectTapGestures(
-                    onPress = {
-                        isTouchOnUpperSide = it.y > centerHeight
-                    }
-                )
-            }
-            .pointerInput(Unit) {
-                coroutineScope {
-                    while (true) {
-                        val pointerId = awaitPointerEventScope { awaitFirstDown().id }
-                        offsetX.stop()
-                        awaitPointerEventScope {
-                            drag(pointerId) { change ->
-                                val xOffset = offsetX.value + change.positionChange().x
-                                val yOffset = offsetY.value + change.positionChange().y
-                                launch {
-                                    offsetX.snapTo(xOffset)
-                                    offsetY.snapTo(yOffset)
-                                    rotation.snapTo(
-                                        offsetX.value * 0.01f *
-                                                if (isTouchOnUpperSide) -1f else 1f
-                                    )
-                                }
-                                change.consume()
-                            }
-                        }
-                        launch { offsetX.animateTo(0f) }
-                        launch { offsetY.animateTo(0f) }
-                        launch { rotation.animateTo(0f) }
-                    }
-                }
-            }
             .width(400.dp)
             .height(650.dp)
     ) {
